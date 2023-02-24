55ea44cddb7cbdc4f8a0c7a4a64f745c233d0a83
MathumithanRasiah <113061995+MathumithanRasiah@users.noreply.github.com>
2023-02-23 22:10:22 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,32 +3,65 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	puissance = ((voltage ** 2) / resistance)
+	return puissance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
+	produit_scalaire = (v1[0]*v2[0])+(v1[1]*v2[1])
+
+	if produit_scalaire == 0:
+		return True
+
+	else:
+		return False
+
 	pass
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	somme = 0
+	nb_values = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >=0:
+			somme = somme + v
+			nb_values +=1
+
+	average = (somme/nb_values)
+	print(average)
+
+	pass # La variable v contient une valeur de la liste.
 
 def bills(value):
+	twenties = tens = fives = ones = 0
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
 	while value != 0:
 		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+			twenties += value // 20
+			value = value % 20
 
-	return (twenties, tens, fives, twos, ones);
+
+		elif value >= 10:
+
+			tens += value // 10
+			value = value % 10
+
+
+		elif value >= 5:
+
+			fives += value //5
+			value = value % 5
+
+
+		elif value >= 1:
+
+			ones += value // 1
+			value = 0
+
+
+	return (twenties, tens, fives, ones)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -36,11 +69,12 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
-		pass
+		digit_value = abs_value % base
+		result += digit_letters[digit_value]
 	if value < 0:
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+		result += '-'
+	return result[::-1]
 
 
 if __name__ == "__main__":
